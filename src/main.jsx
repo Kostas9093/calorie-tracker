e23ffe606f70eaf0398e9b27092d27588afc9018
Kostas <159778628+Kostas9093@users.noreply.github.com>
2025-07-12 10:49:24 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import DayDetail from './DayDetail';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <Router basename="/calorie-tracker">
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
